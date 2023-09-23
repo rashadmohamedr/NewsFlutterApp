dc3270e096c065c4c75858890ac1cc3d4649914a
KennAdams <kenn.adams200@gmail.com>
2023-09-23 03:10:32 +0300

    changed newsdata in data.dart file to late type

--- news_app/lib/pages/data.dart
+++ news_app/lib/pages/data.dart
@@ -1,28 +1,34 @@
-Map newsData={
+late Map newsData = {
   "breakingNews": [
     {
       "title": "Grand jury wanted charges against Graham, Loeffler and others",
-      "subTitle": "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
+      "subTitle":
+          "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
       "category": "US&Canada",
-      "BackImagePath": "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
+      "BackImagePath":
+          "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
       "date": "",
       "author": "BBC",
       "authorImagePath": ""
     },
     {
       "title": "Grand jury wanted charges against Graham, Loeffler and others",
-      "subTitle": "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
+      "subTitle":
+          "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
       "category": "US&Canada",
-      "BackImagePath": "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
+      "BackImagePath":
+          "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
       "date": "",
       "author": "BBC",
       "authorImagePath": ""
     },
     {
       "title": "Grand jury wanted charges against Graham, Loeffler and others",
-      "subTitle": "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
+      "subTitle":
+          "A new report lays bare the thinking of the Georgia grand jury Trump investigation on election meddling.",
       "category": "US&Canada",
-      "BackImagePath": "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
+      "BackImagePath":
+          "https://ichef.bbc.co.uk/wwhp/999/cpsprodpb/0823/production/_131038020_graham_976getty.jpg",
       "date": "",
       "author": "BBC",
       "authorImagePath": ""
